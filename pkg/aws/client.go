d00929c194d4ded969d40cf6530499125dacf94d
Colin Walker <colinwalker@Colins-MBP.hitronhub.home>
2018-02-18 15:31:43 -0800

    Add aws client.

---
+++ pkg/aws/client.go
@@ -0,0 +1,130 @@
+package aws
+
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/autoscaling/autoscalingiface"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+)
+
+var createSession = session.NewSession
+
+type Client interface {
+	Hostname() string
+	InstanceID() string
+	Region() string
+	GroupName() string
+
+	GroupInstances() (map[string]string, error)
+}
+
+func NewClient() (Client, error) {
+	sess, err := createSession()
+	if err != nil {
+		return nil, err
+	}
+	meta := ec2metadata.New(sess)
+	doc, err := meta.GetInstanceIdentityDocument()
+	if err != nil {
+		return nil, err
+	}
+	hostname, err := meta.GetMetadata("local-hostname")
+	if err != nil {
+		return nil, err
+	}
+	instanceID, err := meta.GetMetadata("instance-id")
+	if err != nil {
+		return nil, err
+	}
+	sess, err = createSession(&aws.Config{
+		Region: &doc.Region,
+	})
+	c := &client{
+		asg:        autoscaling.New(sess),
+		ec2:        ec2.New(sess),
+		hostname:   hostname,
+		region:     doc.Region,
+		instanceID: instanceID,
+	}
+	err = c.loadGroupName()
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+type client struct {
+	asg        autoscalingiface.AutoScalingAPI
+	ec2        ec2iface.EC2API
+	hostname   string
+	region     string
+	instanceID string
+	groupName  string
+}
+
+func (c *client) Region() string     { return c.region }
+func (c *client) Hostname() string   { return c.hostname }
+func (c *client) InstanceID() string { return c.instanceID }
+func (c *client) GroupName() string  { return c.groupName }
+
+func (c *client) loadGroupName() error {
+	var name string
+	err := c.asg.DescribeAutoScalingGroupsPages(
+		&autoscaling.DescribeAutoScalingGroupsInput{},
+		func(page *autoscaling.DescribeAutoScalingGroupsOutput, lastPage bool) bool {
+			for _, g := range page.AutoScalingGroups {
+				for _, inst := range g.Instances {
+					if c.instanceID == *inst.InstanceId {
+						name = *g.AutoScalingGroupName
+						return false
+					}
+				}
+			}
+			return true
+		},
+	)
+	if err != nil {
+		return err
+	}
+	if name == "" {
+		return errors.New("aws: autoscaling group not found")
+	}
+	c.groupName = name
+	return nil
+}
+
+func (c *client) GroupInstances() (map[string]string, error) {
+	groups, err := c.asg.DescribeAutoScalingGroups(&autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{&c.groupName},
+	})
+	if err != nil {
+		return nil, err
+	}
+	instances := []*string{}
+	for _, group := range groups.AutoScalingGroups {
+		for _, inst := range group.Instances {
+			instances = append(instances, inst.InstanceId)
+		}
+	}
+	ec2Inst, err := c.ec2.DescribeInstances(&ec2.DescribeInstancesInput{
+		InstanceIds: instances,
+	})
+	if err != nil {
+		return nil, err
+	}
+	out := map[string]string{}
+	for _, rev := range ec2Inst.Reservations {
+		for _, inst := range rev.Instances {
+			if len(inst.NetworkInterfaces) == 0 {
+				continue
+			}
+			out[*inst.InstanceId] = *inst.NetworkInterfaces[0].PrivateDnsName
+		}
+	}
+	return out, nil
+}
---
+++ pkg/aws/client_test.go
@@ -0,0 +1,155 @@
+package aws
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/autoscaling/autoscalingiface"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+var mockSession = func() *session.Session {
+	// server is the mock server that simply writes a 200 status back to the client
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{}`))
+	}))
+
+	return session.Must(session.NewSession(&aws.Config{
+		DisableSSL: aws.Bool(true),
+		Endpoint:   aws.String(server.URL),
+		Region:     aws.String("test"),
+	}))
+}()
+
+type EC2Mock struct {
+	ec2iface.EC2API
+	mock.Mock
+}
+
+func (m *EC2Mock) DescribeInstances(in *ec2.DescribeInstancesInput) (
+	*ec2.DescribeInstancesOutput, error) {
+	a := m.Called(in)
+	return a.Get(0).(*ec2.DescribeInstancesOutput), a.Error(1)
+}
+
+type ASGMock struct {
+	autoscalingiface.AutoScalingAPI
+	mock.Mock
+}
+
+func (m *ASGMock) DescribeAutoScalingGroups(
+	in *autoscaling.DescribeAutoScalingGroupsInput) (
+	*autoscaling.DescribeAutoScalingGroupsOutput, error) {
+	a := m.Called(in)
+	return a.Get(0).(*autoscaling.DescribeAutoScalingGroupsOutput), a.Error(1)
+}
+
+func (m *ASGMock) DescribeAutoScalingGroupsPages(
+	in *autoscaling.DescribeAutoScalingGroupsInput,
+	fn func(*autoscaling.DescribeAutoScalingGroupsOutput, bool) bool) error {
+	a := m.Called(in)
+	fn(a.Get(0).(*autoscaling.DescribeAutoScalingGroupsOutput), true)
+	return a.Error(1)
+}
+
+func TestClient_Load(t *testing.T) {
+	createSession = func(...*aws.Config) (*session.Session, error) { return mockSession, nil }
+	_, err := NewClient()
+	require.Equal(t, "aws: autoscaling group not found", err.Error())
+}
+
+func TestClient_Instances(t *testing.T) {
+	a := &ASGMock{}
+	e := &EC2Mock{}
+
+	c := &client{
+		asg:        a,
+		ec2:        e,
+		hostname:   "1.ec2.internal",
+		region:     "us-west-2",
+		instanceID: "1",
+		groupName:  "test",
+	}
+
+	a.On("DescribeAutoScalingGroups", &autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{aws.String("test")},
+	}).Return(&autoscaling.DescribeAutoScalingGroupsOutput{
+		AutoScalingGroups: []*autoscaling.Group{
+			{
+				Instances: []*autoscaling.Instance{
+					{InstanceId: aws.String("1")},
+					{InstanceId: aws.String("2")},
+				},
+			},
+		},
+	}, nil)
+	e.On("DescribeInstances", &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String("1"), aws.String("2")},
+	}).Return(&ec2.DescribeInstancesOutput{
+		Reservations: []*ec2.Reservation{{
+			Instances: []*ec2.Instance{
+				{
+					InstanceId: aws.String("1"),
+					NetworkInterfaces: []*ec2.InstanceNetworkInterface{{
+						PrivateDnsName: aws.String("1.ec2.internal"),
+					}},
+				},
+				{
+					InstanceId: aws.String("2"),
+					NetworkInterfaces: []*ec2.InstanceNetworkInterface{{
+						PrivateDnsName: aws.String("2.ec2.internal"),
+					}},
+				},
+			},
+		}},
+	}, nil)
+
+	m, err := c.GroupInstances()
+	require.Nil(t, err)
+	require.Equal(t, map[string]string{
+		"1": "1.ec2.internal",
+		"2": "2.ec2.internal",
+	}, m)
+
+	e.AssertExpectations(t)
+	a.AssertExpectations(t)
+}
+
+func TestClient_LoadName(t *testing.T) {
+	a := &ASGMock{}
+
+	c := &client{
+		asg:        a,
+		hostname:   "1.ec2.internal",
+		region:     "us-west-2",
+		instanceID: "1",
+		groupName:  "test",
+	}
+
+	a.On("DescribeAutoScalingGroupsPages",
+		&autoscaling.DescribeAutoScalingGroupsInput{}).
+		Return(&autoscaling.DescribeAutoScalingGroupsOutput{
+			AutoScalingGroups: []*autoscaling.Group{
+				{
+					AutoScalingGroupName: aws.String("new"),
+					Instances: []*autoscaling.Instance{
+						{InstanceId: aws.String("1")},
+						{InstanceId: aws.String("2")},
+					},
+				},
+			},
+		}, nil)
+
+	err := c.loadGroupName()
+	require.Nil(t, err)
+	require.Equal(t, "new", c.groupName)
+
+	a.AssertExpectations(t)
+}
